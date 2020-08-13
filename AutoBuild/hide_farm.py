27aeda533e886ee0e8bc696e71a2ceb242e7b1f3
tasi788 <tdc@tdccc.com.tw>
2020-08-13 13:19:20 +0800

    ✨ 我讓他變厲害ㄌ

--- AutoBuild/hide_farm.py
+++ AutoBuild/hide_farm.py
@@ -1,3 +1,8 @@
+import os
+from datetime import datetime, timedelta, timezone
+
+tz = timezone(timedelta(hours=+8))
+
 domain_list = ''
 with open('../nofarm_hosts.txt', 'r') as files:
     for domains in files.read().split('\n'):
@@ -7,5 +12,32 @@ with open('../nofarm_hosts.txt', 'r') as files:
                 domain=domain
             )
 
+if not os.path.exists('../hide_farm_from_search.txt'):
+    with open('../hide_farm_from_search.txt', 'w') as files:
+        pass
+
+with open('../hide_farm_from_search.txt', 'r') as orig:
+    version, ver_dt = '', ''
+    try:
+        version = orig.read().split('\n')[2].split(': ')[1]
+        ver_dt = datetime.strptime(version[:-3], '%Y.%m%d').date()
+    except IndexError:
+        pass
+
+    now = datetime.now(tz)
+    v = now.strftime('%Y.%m%d.')
+    if now.date() != ver_dt:
+        v += '01'
+    else:
+        v += str(int(version[-2:])+1).zfill(2)
+
 with open('../hide_farm_from_search.txt', 'w') as files:
-    files.write(domain_list)
+    head = '[Adblock Plus]\n' \
+           '! Title: Make futa great again!\n' \
+           '! Version: {version}\n' \
+           '! Expires: 1 hour\n' \
+           '! Homepage: https://t.me/adguard_tw\n' \
+           '! ----------------------------------------------------------------------\n'.format(
+               version=v
+           )
+    files.write(head + domain_list)
