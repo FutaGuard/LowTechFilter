12ec32f615fd45ddeeb30710e221d41fd2243c27
踢低吸 <tdc@sudo.host>
2022-08-31 04:28:26 +0000

    🐛 action 去除符號

--- AutoBuild/pure_domain.py
+++ AutoBuild/pure_domain.py
@@ -1,5 +1,11 @@
+import re
+
 with open('../hosts.txt', 'r') as files:
     data = files.read().splitlines()
-    output = '\n'.join(e for e in list(filter(lambda x: not x.startswith('/') and not x.startswith('! regex'), data)))
-    with open('../domains.txt', 'w') as newoutput:
-        newoutput.write(output)
+    newdata = '\n'.join(data[5:])
+    desc = '\n'.join(x.replace('!', '#') for x in data[:5]) + '\n'
+    
+    with open('../domains.txt', 'w') as output:
+        pattern = r'(?<=^\|\|)\S+\.\S{2,}(?=\^)'
+        desc += '\n'.join(re.findall(pattern, newdata, re.MULTILINE))
+        output.write(desc)
