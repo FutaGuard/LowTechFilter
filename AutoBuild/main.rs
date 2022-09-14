bb09f9e3efb0b58139b8e767fb5fd1e92ec5b324
tdc <tdc@tdccc.com.tw>
2022-09-14 17:32:59 +0800

    ⚡️ 肚子好餓回家吃飯
    隨便寫了一下了解個 rustc

--- AutoBuild/main.rs
+++ AutoBuild/main.rs
@@ -1,34 +1,26 @@
-fn main() {
-    struct FilterList<'a> {
-        abp: Vec<&'a str>,
-        hosts: Vec<&'a str>,
-    }
-    struct HEAD<'a> {
-        abp: &'a str,
-        hosts: &'a str,
-    }
+use std::collections::HashMap;
 
-    let filterlist = FilterList {
-        abp: vec!["experimental.txt", "filter.txt"],
-        hosts: vec!["hosts.txt", "nofarm_hosts.txt"],
-    };
-    let url: &str = "https://filter.futa.gg/";
-    let head = HEAD {
-        abp: "[Adblock Plus]\n
-             ! Title: LowTechFilter {name}\n
-             ! Version: {version}\n
-             ! Expires: 1 hour\n
-             ! Homepage: https://t.me/AdBlock_TW\n
-             ! ----------------------------------------------------------------------\n",
-        hosts: "! FutaHosts\n\
-               ! LowTechFilter {name}\n\
-               ! URL: <https://github.com/FutaGuard/LowTechFilter>\n\
-               ! Version: {version}\n\
-               ! --------------------------------------------------\n",
-    };
-    println!("{:?}", head.hosts);
-    for val in head {
-        println!("{:?}", val)
+fn main() {
+    let mut filterlist = HashMap::new();
+    let mut head = HashMap::new();
+    filterlist.insert(String::from("abp"), ["experimental.txt", "filter.txt"]);
+    filterlist.insert(String::from("hosts"), ["hosts.txt", "nofarm_hosts.txt"]);
+
+    head.insert(String::from("abp"), "[Adblock Plus]\n
+                                     ! Title: LowTechFilter {name}\n
+                                     ! Version: {version}\n
+                                     ! Expires: 1 hour\n
+                                     ! Homepage: https://t.me/AdBlock_TW\n
+                                     ! ----------------------------------------------------------------------\n");
+    head.insert(String::from("hosts"), "! FutaHosts\n\
+                                       ! LowTechFilter {name}\n\
+                                       ! URL: <https://github.com/FutaGuard/LowTechFilter>\n\
+                                       ! Version: {version}\n\
+                                       ! --------------------------------------------------\n");
+    let _url: &str = "https://filter.futa.gg/";
+
+    for category in filterlist {
+        println!("{:?}", category);
     }
     // println!("{:?}", filterlist::field_names());
 }
