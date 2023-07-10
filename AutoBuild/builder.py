414a9df454c340f2b28fe60053ebba53b9931411
踢低吸 <tdc@sudo.host>
2023-07-10 07:56:26 +0000

    🐛 typo

--- AutoBuild/builder.py
+++ AutoBuild/builder.py
@@ -85,7 +85,7 @@ for category in filterlist:
                 newdata = '\n'.join(data[5:])
                 desc = '\n'.join(x.replace('!', '#') for x in data[:5]) + '\n'
 
-                name = filename.split('.text')[0]
+                name = filename.split('.txt')[0]
                 with open(name+'-domains.txt', 'w') as output:
                     pattern = r'(?<=^\|\|)\S+\.\S{2,}(?=\^)'
                     desc += '\n'.join(re.findall(pattern, newdata, re.MULTILINE))
